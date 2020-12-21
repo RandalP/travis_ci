2015eba25edb9365fde5bd629c4b107847e627f7
Randal Parsons <Randal.Parsons@lucidogroup.io>
2020-12-21 17:51:22 +1100

    Add NAME variable into message

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const NAME=process.env.NAME;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log(`${NAME} rules!`);
     await sleep(5000);
   }
 }
